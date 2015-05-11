9d2983441e95f04991801ac202d9571b4cfdc834
Tommy Inouye <ti2181@columbia.edu>
2015-05-11 17:29:57 -0400

    got rid of duplicate file

--- ti2181_hw4 2/hw4/q5.sh
+++
@@ -1,7 +0,0 @@
-# /bin/bash
-python q5.py tag_train.dat suffix_tagger.model
-bash q4.sh suffix_tagger.model
-python eval_tagger.py tag_dev.key tag_dev.out 
-
-
-
--- ti2181_hw4 2/hw4/q6.sh
+++
@@ -1,7 +0,0 @@
-# /bin/bash
-python q6.py tag_train.dat $1 $2
-bash q4.sh $1
-python eval_tagger.py tag_dev.key tag_dev.out 
-
-
-
